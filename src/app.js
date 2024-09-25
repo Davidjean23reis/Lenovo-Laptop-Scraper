37e2264749755cfc64632e167b629a3016c8a97c
David Jean <david.jean.reis23@gmail.com>
2024-09-24 21:09:19 -0300

    Alterei o nome do meu script no arquivo package.json de  index.js para  app.js também alterei  o caminho  devido a pasta criada SRC

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res)=>{
- res.send('Hello Word!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello Word!");
+});
 
-app.listen(port, () =>{
-    console.log(`Servidor rodando no endereço  http://localhost:3000`)
-})
+app.listen(port, () => {
+  console.log(`Servidor rodando no endereço  http://localhost:3000`);
+});
